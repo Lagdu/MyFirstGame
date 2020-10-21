b7a39beae00c0077d2a0fdb295b9d5aa767c426e
Lagdu <maxime.tohier@gmail.com>
2020-10-21 23:36:18 +0200

    Chapitre 19 OK

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject settingsMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    public void Paused()
+    {
+        settingsMenu.SetActive(false);
+        PlayerMovement.instance.enabled = false;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+
+
+    public void Resume()
+    {
+        PlayerMovement.instance.enabled = true;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        gameIsPaused = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        DontDestroyOnLoadScene.instance.RemoveFromDontDestroyOnLoad();
+        Resume();
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void SettingsMenu()
+    {
+        settingsMenu.SetActive(true);
+        pauseMenuUI.SetActive(false);
+    }
+}
