3ee10b3969b19a069a71d522787f4422799d0187
Lagdu <maxime.tohier@gmail.com>
2020-10-22 18:33:26 +0200

    Chapitre 29 OK

---
+++ Assets/Script/DialogueManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    private Queue<string> sentences;
+
+    public static DialogueManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d\'une instance DialogueManager dans la sscene");
+            return;
+        }
+
+        instance = this;
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach (string sentense in dialogue.sentences)
+        {
+            sentences.Enqueue(sentense);
+        }
+
+        DisplayNextSentence();
+        
+    }
+
+    void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    public void EndDialogue()
+    {
+        Debug.LogWarning("Fin");
+    }
+
+}
